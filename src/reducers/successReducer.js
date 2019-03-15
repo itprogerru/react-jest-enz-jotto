e90736dd6e840bbeebeb2d2587ae7c41c05ab987
Vyacheslav Salenok <vyacheslav.salenok@simbirsoft.com>
2019-03-15 15:20:19 +0400

    add test reduces

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -4,6 +4,14 @@
  * @param {object} action -action to be reduced
  * @return {boolean} - new success state
  */
-export default (state, action)=> {
-  return null;
+import { actionTypes } from '../actions'
+
+export default (state = false, action)=> {
+  switch(action.type) {
+    case (actionTypes.CORRECT_GUESS):
+      return true;
+    default:
+      return state;
+  }
+
 }
